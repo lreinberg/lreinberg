733a14b8a70f9631b170b95cee8b58918bba5d61
lreinberg <lreinberg@ethz.ch>
2022-11-22 21:21:43 +0100

    initial commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+Import-Module -Name Terminal-Icons
+oh-my-posh --init --shell pwsh --config "C:\Users\lucas\dev\config\lreinberg.omp.json" | Invoke-Expression
---
+++ Microsoft.VSCode_profile.ps1
@@ -0,0 +1,2 @@
+Import-Module -Name Terminal-Icons
+oh-my-posh --init --shell pwsh --config C:\Users\lucas\.oh-my-posh\themes\lreinberg.omp.json | Invoke-Expression
